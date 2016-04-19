13affa0ba14e45a207abb6b31205008fb2919958
DimaPetrinko <petrinko6@gmail.com>
2016-04-19 17:57:31 +0300

    moving = !moving

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,47 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public Transform head;
+	public float speed = 1;
+
+	private Transform myTransform;
+	private Rigidbody rb;
+	private Vector3 moveVector;
+	private bool moving = false;
+
+	void OnEnable(){
+		Cardboard.SDK.OnTrigger += TriggerPulled;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnDisable(){
+		Cardboard.SDK.OnTrigger -= TriggerPulled;
 	}
+
+	void Start()
+	{
+		myTransform = transform;
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update()
+	{
+		if (moving) {
+			moveVector = (head.forward - Vector3.up * head.forward.y).normalized * speed;
+			//Debug.DrawRay (head.position, moveVector * 10);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (moving)
+		{
+			rb.MovePosition (myTransform.position + (moveVector * Time.fixedDeltaTime));
+		}
+	}
+
+	void TriggerPulled() {
+		//Debug.Log("The trigger was pulled!");
+		moving = !moving;
+	}
+
 }
